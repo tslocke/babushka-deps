21c33035c9b39a1d558ed7305b2dde6777a1dcec
Tom Locke <tom@tomlocke.com>
2010-10-13 06:09:40 -0700

    Add benhoskings source name

--- rails.rb
+++ rails.rb
@@ -1,5 +1,5 @@
 dep "rails-db-configured.mysql" do
-  requires 'mysql.gem', 'mysql access'
+  requires 'benhoskings:mysql.gem', 'benhoskings:mysql access'
   met? { File.directory? var(:rails_root) / 'config/database.yml' }
   meet {
     render_erb "rails/database.yml.erb", :to => "#{var :rails_root}/config/database.yml", :perms => '755'
